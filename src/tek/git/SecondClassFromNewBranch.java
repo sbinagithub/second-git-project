70ffb3fa1aaf6cab2fa15f74defe3ba719335c06
shabnam <shabnambina1012@gmail.com>
2023-08-16 16:05:13 -0400

    usdated existing class

---
+++ src/tek/git/SecondClassFromNewBranch.java
@@ -0,0 +1,12 @@
+package tek.git;
+
+public class SecondClassFromNewBranch {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is changes from my branch");
+		System.out.println("This is update from new branch");
+
+	}
+
+}
